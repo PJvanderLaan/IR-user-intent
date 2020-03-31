544977f85973ed119930661430f49bb07b909c4e
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-03-31 11:49:28 +0200

    json file reading

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import csv
+import json
+
+DATA_PATH = './data/MSDialog/MSDialog-Intent.json'
+
+def load_data(data_path=DATA_PATH):
+    with open(data_path, mode='r') as json_file:
+        return json.load(json_file)
+
+if __name__ == "__main__":
+    print(load_data())
