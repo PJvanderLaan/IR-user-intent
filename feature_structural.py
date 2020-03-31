5fc1139c9b4621983e3424ee0f491a8074234145
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-03-31 13:17:10 +0200

    Refactors

--- feature_structural.py
+++
@@ -1,5 +0,0 @@
-def calculate_structural_features(data)
-	
-
-if __name__ == "__main__":
-	
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from util.msdialog_data_helper import parse_data, print_data_analytics
+from features.feature_structural import calculate_structural_features
 
 DATA_PATH = './data/MSDialog/MSDialog-Intent.json'
 
@@ -7,7 +8,12 @@ def load_data(data_path=DATA_PATH):
     with open(data_path, mode='r') as json_file:
         return json.load(json_file)
 
+def feature_analysis(json_data):
+    structural_features = calculate_structural_features(json_data)
+
 if __name__ == "__main__":
     json_data = load_data()
     all_utterances, all_isUsers, all_tags = parse_data(json_data)
+
     print_data_analytics(all_utterances, all_isUsers, all_tags)
+    feature_analysis(json_data)
