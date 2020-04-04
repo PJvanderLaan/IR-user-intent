051e08d1db95a4c4e6304ec3add270011243d2f7
Philippe Lammerts <p.m.lammerts@student.tudelft.nl>
2020-04-04 21:15:13 +0200

    Add content features

--- features/feature_content.py
+++ features/feature_content.py
@@ -1,12 +1,14 @@
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
 from util.msdialog_data_helper import parse_data, print_data_analytics, appendAll
 from util.pickle_helper import get_pickled_data, store_data_as_pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+import pandas as pd
 
 # Path to content feature pickle
 CACHED_DATA_FILENAME = 'content_pickle_data'
+
+KEYWORDS = ["what", "where", "when", "why", "who", "how"]
 
 
 # Calculates utterance similarity score between itself and the first utterance of the dialog.
@@ -17,6 +19,7 @@ def get_utterance_similarity(json_data):
 
         utterances = [item["utterance"] for item in dialog_data]
         tfidf = compute_cosine_similarity(utterances)
+        # Get the cosine similarity score between each utterance and the first utterance of the dialog.
         for i in range(1, tfidf.shape[0]):
             results.append(tfidf[0, i])
 
@@ -31,6 +34,7 @@ def get_dialog_similarity(json_data):
 
         utterances = [item["utterance"] for item in dialog_data]
         tfidf = compute_cosine_similarity(utterances)
+        # Compute the average cosine similarity score between each utterance and the entire dialog.
         for i in range(tfidf.shape[0]):
             sum = 0
             for j in range(tfidf.shape[0]):
@@ -41,7 +45,7 @@ def get_dialog_similarity(json_data):
     return results
 
 
-# Computes the cosine similarity score for a set of utterances.
+# Computes the cosine similarity scores for a set of utterances.
 def compute_cosine_similarity(utterances):
     tfidf = TfidfVectorizer().fit_transform(utterances)
     return tfidf * tfidf.T
@@ -52,11 +56,42 @@ def get_question_mark(json_data):
     all_utterances, _, _, _ = parse_data(json_data)
     results = []
 
-    for x in all_utterances:
-        if "?" in x:
+    for utterance in all_utterances:
+        if "?" in utterance:
             results.append(1)
         else:
             results.append(0)
+
+    return results
+
+
+# Get features that indicate duplication by checking 'same' and 'similar' words.
+def get_duplicate(json_data):
+    all_utterances, _, _, _ = parse_data(json_data)
+    results = []
+
+    for utterance in all_utterances:
+        if "same" or "similar" in utterance.lower():
+            results.append(1)
+        else:
+            results.append(0)
+
+    return results
+
+
+# Check for the 5W1H keywords in the utterances.
+def get_keywords(json_data):
+    all_utterances, _, _, _ = parse_data(json_data)
+    results = []
+
+    for utterance in all_utterances:
+        keywords = [0] * 6
+
+        for index, keyword in enumerate(KEYWORDS):
+            if keyword in utterance.lower():
+                keywords[index] = 1
+
+        results.append(keywords)
 
     return results
 
@@ -66,10 +101,10 @@ def calculate_content_features(json_data):
     utterance_similarity = get_utterance_similarity(json_data)
     dialog_similarity = get_dialog_similarity(json_data)
     question_mark = get_question_mark(json_data)
-    duplicate = 1
-    utterance_keywords = 1
+    duplicate = get_duplicate(json_data)
+    keywords = get_keywords(json_data)
 
-    return utterance_similarity, dialog_similarity, question_mark, duplicate, utterance_keywords
+    return utterance_similarity, dialog_similarity, question_mark, duplicate, keywords
 
 
 # Calculate all features and store it in a pickle file
