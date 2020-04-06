82e606a5b2bbfa7ff83d6966d7dd1f94c28ea9ff
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-04-06 18:40:34 +0200

    Add chaining with grid search

--- classifier_chaining.py
+++ classifier_chaining.py
@@ -9,22 +9,38 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from util.pickle_helper import get_pickled_data, store_data_as_pickle
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 CACHED_DATA_FILENAME = 'fitted_chain'
 
 def chaining_svm(X, Y, C=0.5, gamma='scale', max_iter=-1, folds=10):
 	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.2, random_state=0)
 	# base_clf = SVC(C=C, kernel='rbf', gamma=gamma, max_iter=max_iter)
-	base_clf = SVC()
-	
-	chain = get_pickled_data(f'testchainpickle')
-	# chain = ClassifierChain(base_clf, cv=folds, order='random', random_state=0)
+
+	tuned_parameters = [
+		{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]}, 
+		{'kernel': ['linear'], 'C': [1, 10, 100, 1000]}
+	]
+
+	# base_clf = SVC(C=10)
+	base_clf = GridSearchCV(
+        SVC(), tuned_parameters
+    )
+
+	# chain = get_pickled_data(f'testchainpickle')
+	print('calculating chain')
+	chain = ClassifierChain(base_clf, cv=folds, order='random', random_state=0)
+	print('fitting')
 	chain.fit(X_train, Y_train)
 	y_pred = chain.predict(X_test)
+
 	print(f'accuracy: {accuracy_score(Y_test, y_pred)}')
 	print(f'f1 score: {f1_score(Y_test, y_pred, average="macro")}')
 	print(f'precision: {precision_score(Y_test, y_pred, average="macro")}')
 	print(f'recall_score: {recall_score(Y_test, y_pred, average="macro")}')
+	classification_report(Y_test, y_pred)
+
 	store_data_as_pickle(chain, f'testchainpickle')
 
 
