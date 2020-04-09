f4fbdd361591493fbd6e7fd4495d41964a927e7a
Philippe Lammerts <p.m.lammerts@student.tudelft.nl>
2020-04-09 13:37:09 +0200

    Add evaluation fil

--- classifier/knn.py
+++ classifier/knn.py
@@ -1,7 +1,5 @@
 from sklearn import neighbors
-from sklearn.metrics import multilabel_confusion_matrix, accuracy_score, precision_score, recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import average_precision_score
+
 
 def knn_classifier(trainX, trainY, n_neighbours):
     clf = neighbors.KNeighborsClassifier(n_neighbours)
@@ -11,66 +9,3 @@ def knn_classifier(trainX, trainY, n_neighbours):
 
 def knn_predict(clf, dataX):
     return clf.predict(dataX)
-
-
-def knn_get_accuracy(testY, predY):
-    accuracies = []
-    for label_index, singleY in enumerate(testY):
-        correct = 0
-        union = 0
-        for i, value in enumerate(singleY):
-            if value == True and predY[label_index][i] == True:
-                correct = correct + 1
-
-            if value == True or predY[label_index][i] == True:
-                union = union + 1
-        accuracies.append(correct/union)
-
-    return sum(accuracies)/len(accuracies)
-
-
-def knn_get_f1(testY, predY):
-    precision = knn_get_precision(testY, predY)
-    recall = knn_get_recall(testY, predY)
-
-    return 2 * ((precision * recall)/(precision + recall))
-
-
-def knn_get_precision(testY, predY):
-    precisions = []
-    for label_index, singleY in enumerate(testY):
-        correct = 0
-        predicted = 0
-        for i, value in enumerate(singleY):
-            if value == True and predY[label_index][i] == True:
-                correct = correct + 1
-
-            if predY[label_index][i] == True:
-                predicted = predicted + 1
-        if (predicted != 0):
-            precisions.append(correct/predicted)
-        # else:
-        #     precisions.append(0)
-
-    return sum(precisions)/len(precisions)
-
-
-def knn_get_recall(testY, predY):
-    precisions = []
-    for label_index, singleY in enumerate(testY):
-        correct = 0
-        true_labels = 0
-        for i, value in enumerate(singleY):
-            if value == True and predY[label_index][i] == True:
-                correct = correct + 1
-
-            if testY[label_index][i] == True:
-                true_labels = true_labels + 1
-
-        precisions.append(correct/true_labels)
-
-    return sum(precisions)/len(precisions)
-
-
-def knn_get_confusion_matrix(clf, testY, predY):
-    return multilabel_confusion_matrix(testY, predY)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from scipy import sparse
 import numpy as np
 import random
 from sklearn.model_selection import KFold
-from classifier.knn import knn_classifier, knn_predict, knn_get_accuracy, knn_get_recall, knn_get_precision, knn_get_f1
+
+from classifier.evaluation import get_precision, get_recall, get_f1, get_accuracy
+from classifier.knn import knn_classifier, knn_predict
 from features.feature_content import fetch_content_features_pickle, calculate_and_store_content_as_pickle
 from features.feature_sentiment import calculate_sentimental_features, calculate_and_store_sentiment_as_pickle, \
     fetch_sentiment_features_pickle
@@ -67,22 +69,23 @@ def construct_data(json_data):
 
 if __name__ == "__main__":    
     json_data = load_data()
+
     # calculate_and_store_content_as_pickle(json_data)
     # calculate_and_store_sentiment_as_pickle(parse_data(json_data))
     # calculate_and_store_as_pickle(json_data)
 
-    # X_csr_train, Y_csr_train, X_np_train, Y_np_train = construct_data(json_data)
-    # kf = KFold(n_splits=5)
-    # kf.get_n_splits(X_np_train)
-    # for train_index, test_index in kf.split(X_np_train):
-    #     print("TRAIN:", train_index, "TEST:", test_index)
-    #     X_train, X_test = X_np_train[train_index], X_np_train[test_index]
-    #     y_train, y_test = Y_np_train[train_index], Y_np_train[test_index]
-    #     knn = knn_classifier(X_train, y_train, 3)
-    #     knn_pred = knn_predict(knn, X_test)
-    #     knn_prec = knn_get_precision(y_test, knn_pred)
-    #     knn_recall = knn_get_recall(y_test, knn_pred)
-    #     knn_f1 = knn_get_f1(y_test, knn_pred)
-    #     knn_acc = knn_get_accuracy(y_test, knn_pred)
-    #     print("Acc:{}\tRecall:{}\tPrecision:{}\tF1-score:{}".format(knn_acc, knn_recall, knn_prec, knn_f1))
-    # pass
+    X_csr_train, Y_csr_train, X_np_train, Y_np_train = construct_data(json_data)
+    kf = KFold(n_splits=5)
+    kf.get_n_splits(X_np_train)
+    for train_index, test_index in kf.split(X_np_train):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X_np_train[train_index], X_np_train[test_index]
+        y_train, y_test = Y_np_train[train_index], Y_np_train[test_index]
+        knn = knn_classifier(X_train, y_train, 3)
+        knn_pred = knn_predict(knn, X_test)
+        knn_prec = get_precision(y_test, knn_pred)
+        knn_recall = get_recall(y_test, knn_pred)
+        knn_f1 = get_f1(y_test, knn_pred)
+        knn_acc = get_accuracy(y_test, knn_pred)
+        print("Acc:{}\tRecall:{}\tPrecision:{}\tF1-score:{}".format(knn_acc, knn_recall, knn_prec, knn_f1))
+    pass
