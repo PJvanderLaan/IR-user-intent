eff7017bf5f8f45874bb5d3addbf9dce72168a13
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-04-09 12:09:20 +0200

    add new function

--- util/msdialog_data_helper.py
+++ util/msdialog_data_helper.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from itertools import groupby
 
 # Append all items of an array to an array
 def appendAll(array, items):
@@ -20,7 +21,7 @@ def fetch_labels(json_data, mapping=['OQ', 'RQ', 'CQ', 'FD', 'FQ', 'IR', 'PA', '
 			labels = item["tags"].split(" ")
 			labels = list(filter(lambda x: x in mapping, labels))
 			one_hot_encoding = [x in labels for x in mapping]
-			clean_one_hot_encoding  = [x in labels and x not in blacklist_mapping for x in mapping]
+			clean_one_hot_encoding = [x in labels and x not in blacklist_mapping for x in mapping]
 			
 			if (clean_one_hot_encoding.count(1) > 0):
 				all_labels.append(clean_one_hot_encoding)
@@ -56,15 +57,40 @@ def parse_dialog(dialog_dict):
 # Print data analytics
 def print_data_analytics(json_data):
     all_utterances, all_isUsers, all_tags, _ = parse_data(json_data)
+    blacklist_mapping = ['GG', 'JK', 'O']
 
     user_dialog_count = len(list(filter(lambda x: x == True, all_isUsers)))
+    print(all_tags)
     all_tags = [", ".join(items) for items in all_tags]
+
     tags = list(Counter(all_tags).keys())
     counts = list(Counter(all_tags).values())
-    print_number_top_tags = 10
+    print_number_top_tags = 32
 
     print(f'Total rows: {len(all_utterances)}')
     print(f'User: {user_dialog_count}, Agent: {len(all_utterances) - user_dialog_count}')
     print(f'Top {print_number_top_tags} tags with highest frequency:')    
     for i in range(0, print_number_top_tags):
         print(f' {i + 1}. {tags[i]} ({counts[i]})')
+
+def fetch_preprocessed_labels(json_data, number_of_tags=32):
+	others_ohe = [False, False, False, False, False, False, False, False, False, False, False, True]
+	labels = fetch_labels(json_data)
+	counted = Counter(tuple(item) for item in labels)
+	most_frequent = counted.most_common(number_of_tags)
+	most_frequent_list = list(map(lambda x: list(x[0]), most_frequent))
+	most_frequent_list.append(others_ohe)
+	return most_frequent_list
+
+	# tags = list(Counter(tuple(item) for item in labels).keys())
+	# counts = list(Counter(tuple(item) for item in labels).values())
+
+	# print_number_top_tags = 32
+
+	# for i in range(0, print_number_top_tags):
+	# 	print(f' {i + 1}. {tags[i]} ({counts[i]})')
+
+
+
+
+
