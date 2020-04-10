07144759333c13922b3867750d547a1ed5c2d9be
Philippe Lammerts <p.m.lammerts@student.tudelft.nl>
2020-04-10 14:35:48 +0200

    Add all experiments and final rf classifier

--- classifier/random_forest.py
+++ classifier/random_forest.py
@@ -5,11 +5,21 @@ from sklearn.model_selection import GridSearchCV, cross_validate
 import matplotlib.pyplot as plt
 from classifier.evaluation import get_accuracy, get_precision, get_recall, get_f1
 
+scoring = {'Accuracy': make_scorer(get_accuracy),
+           'Precision': make_scorer(get_precision),
+           'Recall': make_scorer(get_recall),
+           'F1-score': make_scorer(get_f1)}
 
-def rf_classifier(trainX, trainY, n_estimators, criterion):
-    clf = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion)
-    clf.fit(trainX, trainY)
+
+def rf_classifier():
+    # Parameter values are the optimal results from the experiments.
+    clf = RandomForestClassifier(n_estimators=100, criterion='entropy', min_samples_split=4, min_samples_leaf=1,
+                                 max_features=None, max_depth=10)
     return clf
+
+
+def evaluate_random_forest(train, test):
+    return cross_validate(rf_classifier(), train, test, cv=10, scoring=scoring)
 
 
 def rf_predict(clf, dataX):
@@ -85,3 +95,95 @@ def calculate_min_samples_split(train, test):
     plt.show()
 
     print(max(accuracy), min_samples_split[accuracy.index(max(accuracy))])
+
+
+# Hyperparam optimization for min_samples_leaf
+def calculate_min_samples_leaf(train, test):
+    min_samples_leaf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+
+    for leaf in min_samples_leaf:
+        print(leaf)
+        clf = RandomForestClassifier(n_estimators=100, criterion='entropy', min_samples_split=4, min_samples_leaf=leaf)
+        values = cross_validate(clf, train, test, cv=10, scoring=scoring)
+        accuracy.append(values['test_Accuracy'].mean())
+        precision.append(values['test_Precision'].mean())
+        recall.append(values['test_Recall'].mean())
+        f1.append(values['test_F1-score'].mean())
+
+    plt.plot(min_samples_leaf, accuracy, label='Accuracy')
+    plt.plot(min_samples_leaf, precision, label='Precision')
+    plt.plot(min_samples_leaf, recall, label='Recall')
+    plt.plot(min_samples_leaf, f1, label='F1-score')
+    plt.ylabel("Accuracy score")
+    plt.xlabel("min_samples_split")
+    plt.title("Minimum required samples for internal node split optimization")
+    plt.legend()
+    plt.show()
+
+    print(max(accuracy), min_samples_leaf[accuracy.index(max(accuracy))])
+
+
+# Hyperparam optimization for max_features
+def calculate_max_features(train, test):
+    max_features = [None, 'auto', 'log2']
+    x = range(3)
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+
+    for feature in max_features:
+        print(feature)
+        clf = RandomForestClassifier(n_estimators=100, criterion='entropy', min_samples_split=4, min_samples_leaf=1,
+                                     max_features=feature)
+        values = cross_validate(clf, train, test, cv=10, scoring=scoring)
+        accuracy.append(values['test_Accuracy'].mean())
+        precision.append(values['test_Precision'].mean())
+        recall.append(values['test_Recall'].mean())
+        f1.append(values['test_F1-score'].mean())
+
+    plt.bar(x, accuracy, 0.4, label='Accuracy')
+    plt.bar(x, precision, 0.3, label='Precision')
+    plt.bar(x, recall, 0.2, label='Recall')
+    plt.bar(x, f1, 0.1, label='F1-score')
+    plt.xticks(x, ['n_features', 'sqrt(n_features)', 'log2(n_features)'])
+    plt.xlabel("max_features setting")
+    plt.title("The number of features to consider for each split")
+    plt.legend()
+    plt.show()
+
+    print(max(accuracy), max_features[accuracy.index(max(accuracy))])
+
+
+# Hyperparam optimization for max_depth
+def calculate_max_depth(train, test):
+    max_depth = [5, 10, 20, 30, 40, 50]
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+
+    for depth in max_depth:
+        print(depth)
+        clf = RandomForestClassifier(n_estimators=100, criterion='entropy', min_samples_split=4, min_samples_leaf=1,
+                                     max_features=None, max_depth=depth)
+        values = cross_validate(clf, train, test, cv=10, scoring=scoring)
+        accuracy.append(values['test_Accuracy'].mean())
+        precision.append(values['test_Precision'].mean())
+        recall.append(values['test_Recall'].mean())
+        f1.append(values['test_F1-score'].mean())
+
+    plt.plot(max_depth, accuracy, label='Accuracy')
+    plt.plot(max_depth, precision, label='Precision')
+    plt.plot(max_depth, recall, label='Recall')
+    plt.plot(max_depth, f1, label='F1-score')
+    plt.xlabel("max_depth")
+    plt.title("The maximum depth of the tree")
+    plt.legend()
+    plt.show()
+
+    print(max(accuracy), max_depth[accuracy.index(max(accuracy))])
--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from sklearn.model_selection import KFold
 from classifier.evaluation import get_precision, get_recall, get_f1, get_accuracy
 from classifier.knn import knn_classifier, knn_predict
 from analysis.feature_importance import analyze_feature_importance
-from classifier.random_forest import rf_classifier, rf_predict, calculate_min_samples_split
+from classifier.random_forest import rf_classifier, rf_predict, calculate_min_samples_split, \
+    calculate_min_samples_leaf, calculate_max_features, calculate_max_depth, evaluate_random_forest
 from features.feature_content import fetch_content_features_pickle, calculate_and_store_content_as_pickle
 from features.feature_sentiment import calculate_sentimental_features, calculate_and_store_sentiment_as_pickle, \
     fetch_sentiment_features_pickle
 from util.msdialog_data_helper import parse_data, print_data_analytics, fetch_labels, fetch_preprocessed_labels, fetch_Y_labels
 from features.feature_structural import calculate_and_store_as_pickle, fetch_structural_features_pickle
+import statistics
 
 DATA_PATH = './data/MSDialog/MSDialog-Intent.json'
 
@@ -106,37 +108,23 @@ if __name__ == "__main__":
 
     X_csr_train, Y_csr_train, X_np_train, Y_np_train = construct_data(json_data)
 
-    calculate_min_samples_split(X_np_train, Y_np_train)
-
     # Feature importance analysis.
     # analyze_feature_importance(X_np_train, Y_np_train, FEATURE_NAMES)
 
-    # kf = KFold(n_splits=5)
-    # kf.get_n_splits(X_np_train)
-    # for train_index, test_index in kf.split(X_np_train):
-    #     print("TRAIN:", train_index, "TEST:", test_index)
-    #     X_train, X_test = X_np_train[train_index], X_np_train[test_index]
-    #     y_train, y_test = Y_np_train[train_index], Y_np_train[test_index]
-    #     knn = knn_classifier(X_train, y_train, 3)
-    #     knn_pred = knn_predict(knn, X_test)
-    #     knn_prec = get_precision(y_test, knn_pred)
-    #     knn_recall = get_recall(y_test, knn_pred)
-    #     knn_f1 = get_f1(y_test, knn_pred)
-    #     knn_acc = get_accuracy(y_test, knn_pred)
-    #     print("Acc:{}\tRecall:{}\tPrecision:{}\tF1-score:{}".format(knn_acc, knn_recall, knn_prec, knn_f1))
-    # pass
+    kf = KFold(n_splits=5)
+    kf.get_n_splits(X_np_train)
+    for train_index, test_index in kf.split(X_np_train):
+        print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X_np_train[train_index], X_np_train[test_index]
+        y_train, y_test = Y_np_train[train_index], Y_np_train[test_index]
+        knn = knn_classifier(X_train, y_train, 3)
+        knn_pred = knn_predict(knn, X_test)
+        knn_prec = get_precision(y_test, knn_pred)
+        knn_recall = get_recall(y_test, knn_pred)
+        knn_f1 = get_f1(y_test, knn_pred)
+        knn_acc = get_accuracy(y_test, knn_pred)
+        print("Acc:{}\tRecall:{}\tPrecision:{}\tF1-score:{}".format(knn_acc, knn_recall, knn_prec, knn_f1))
+    pass
 
-    # kf = KFold(n_splits=10)
-    # kf.get_n_splits(X_np_train)
-    # for train_index, test_index in kf.split(X_np_train):
-    #     print("TRAIN:", train_index, "TEST:", test_index)
-    #     X_train, X_test = X_np_train[train_index], X_np_train[test_index]
-    #     y_train, y_test = Y_np_train[train_index], Y_np_train[test_index]
-    #     rf = rf_classifier(X_train, y_train, 200, 'gini')
-    #     rf_pred = rf_predict(rf, X_test)
-    #     rf_prec = get_precision(y_test, rf_pred)
-    #     rf_recall = get_recall(y_test, rf_pred)
-    #     rf_f1 = get_f1(y_test, rf_pred)
-    #     rf_acc = get_accuracy(y_test, rf_pred)
-    #     print("Acc:{}\tRecall:{}\tPrecision:{}\tF1-score:{}".format(rf_acc, rf_recall, rf_prec, rf_f1))
-    # pass
+    # Evaluation of Random Forest
+    print(evaluate_random_forest(X_np_train, Y_np_train))
